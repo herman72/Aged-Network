50659d0505c5ee78c4ca2d4365bd7f58a844af61
M. Sherafati <m.sherafati7@gmail.com>
2019-03-19 19:54:34 +0330

    For Paper

--- Code/Main_Aged_Networks.py
+++ Code/Main_Aged_Networks.py
@@ -202,7 +202,7 @@ def Mainfunc(thread_no,Node,age,iterate,ensemble,std):
 
 
 
-    np.savetxt('STD100'+ str(thread_no)+str(ens) + '.txt', Mean_Age_ensemble)
+    np.savetxt('STD'+ str(thread_no)+str(ens) + '.txt', Std_Age_ensenble)
     #np.savetxt('LifeTime'+str(thread_no)+'.txt',Time_Itrate)
     #return Time, Mat_Energy, Age_adj,Age_Imshow,Std_Age,Mean_Age,Time_Itrate   
 
@@ -214,7 +214,7 @@ def Mainfunc(thread_no,Node,age,iterate,ensemble,std):
 
 
 Node = 32
-age = 100
+age = 10000
 iterate = 6000
 ensemble = 1
 std = 10
